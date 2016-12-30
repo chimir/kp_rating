ff0a12d417d9d4c3d0ef89ebd1bae6a4fb0b023f
chinir <zhe9191@yandex.ru>
2016-12-30 23:28:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Kinopoisk rating
+
+Небольшой модуль для Drupal 8, выводит рейтинг фильмов с КиноРоиска и IMDb.
+
+## Установка
+
+Установить как обычно. См http://drupal.org/documentation/install/modules-themes/modules-8 для получения дополнительной информации.
+
+## Использование
+
+* Для плей типа **Text (plain)** и **Text (formatted)** появинся новый формат **Kinopoisk rating**
+* В поле необходимо вписать **id** фильма с сайта kinopoisk.ru (пример - `https://www.kinopoisk.ru/film/44218/`, где `44218` id фильма)
---
+++ src/Plugin/Field/FieldFormatter/KpRatingFormatter.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\kp_rating\Plugin\Field\FieldFormatter\KpRatingFormatter.
+ */
+
+namespace Drupal\kp_rating\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Field\FieldItemListInterface;
+
+/** *
+ * @FieldFormatter(
+ *   id = "kp_rating_formatter",
+ *   label = @Translation("Kinopoisk rating"),
+ *   field_types = {
+ *     "text",
+ *     "string"
+ *   }
+ * )
+ */
+class KpRatingFormatter extends FormatterBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = array();
+
+    foreach ($items as $delta => $item) {
+      $film_id = $item->value;
+      $xml_url = "http://rating.kinopoisk.ru/{$film_id}.xml";
+      $xml = simplexml_load_file($xml_url);
+
+      $kp_vote = $xml->kp_rating['num_vote'];
+      $kp_rating = $xml->kp_rating;
+      $imdb_vote = $xml->imdb_rating['num_vote'];
+      $imdb_rating = $xml->imdb_rating;
+
+      $element[$delta] = [
+        '#theme'       => 'kp_formatter',
+        '#kp_vote'     => $kp_vote,
+        '#kp_rating'   => $kp_rating,
+        '#imdb_vote'   => $imdb_vote,
+        '#imdb_rating' => $imdb_rating,
+      ];
+    }
+
+    return $element;
+  }
+
+}
